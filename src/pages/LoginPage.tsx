749d1c023b492603aa27fd5ff232f40320f9a1bc
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2025-04-08 16:34:01 +1000

    Revert "fixed connexion"
    This reverts commit e22cce75219d1e5b1e2b01eff210f41b86d037f0.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -153,12 +153,14 @@ export default function LoginPage() {
             </p>
           </div>
         </form>
-        <Link to="/">
-          <button className="mt-6 w-full flex items-center justify-center text-gray-600 hover:text-gray-800 transition-colors">
-            <ArrowLeft className="h-5 w-5 mr-2" />
-            Retour à l'accueil
-          </button>
-        </Link>
+
+        <button
+          onClick={() => navigate("")}
+          className="mt-6 w-full flex items-center justify-center text-gray-600 hover:text-gray-800 transition-colors"
+        >
+          <ArrowLeft className="h-5 w-5 mr-2" />
+          Retour à l'accueil
+        </button>
       </div>
     </div>
   );
