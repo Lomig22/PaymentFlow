7000b6c842d9cffd54b242929ae4fef0d615ca39
Rino Heinrich <it.specialist.gasca@gmail.com>
2025-05-17 21:48:44 +0300

    Merge branch 'Aroniaina-preprod' of https://github.com/Lomig22/PaymentFlow into Rino-Aftermerge1

--- src/components/settings/SenderSettings.tsx
+++ src/components/settings/SenderSettings.tsx
@@ -260,7 +260,7 @@ export default function SignatureSettings() {
   })();
   return (
     <div className="max-w-7xl mx-auto p-4 space-y-6">
-          {/* --- Erreurs ou succès éventuels --- */}
+          {/* --- Erreurs ou succès éventuels rencontrées --- */}
           {error && (
         <div className="fixed p-4 bg-red-50 border border-red-200 rounded-md text-red-700">
           {error}
