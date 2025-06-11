a29d8c0770e34ab8c8e644f4a56f2b01e4081332
RINO Heinrich <rino@ohmycorp.com>
2025-06-11 10:08:31 +0300

    fix chatling-bot appear after login

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
+import CalendlyAndChatlingLoader from "../src/components/CalendlyAndChatlingLoader"
 import { AbonnementProvider } from "../src/components/context/AbonnementContext.tsx";
 import AppWithMFA from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider defaultColorScheme="light">
+            <CalendlyAndChatlingLoader />
+
       <AbonnementProvider>
-        <AppWithMFA />
+                <AppWithMFA />
       </AbonnementProvider>
     </MantineProvider>
   </StrictMode>
